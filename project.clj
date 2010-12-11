f48edd1947047dc2bd9dab18021733a507014ad8
Gareth Jones <gareth.e.jones@gmail.com>
2010-12-10 19:56:03 -0600

    adding clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clargon "1.0.0-SNAPSHOT"
   :description "command line args done good"
+  :url "https://github.com/gar3thjon3s/clargon"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]]
+  :repositories {"clojars" "http://clojars.org/repo"}
   :main clargon.example)
